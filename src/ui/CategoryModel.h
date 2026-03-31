fd9965d483e8ef17a6db242b59abe6ac0ac34583
ssuyeh <ssuyehwork@gmail.com>
2026-03-31 17:20:34 +0700

    Reconstruct: 2026-03-31 17:20:34

---
+++ src/ui/CategoryModel.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <QStandardItemModel>
+
+namespace ArcMeta {
+
+class CategoryModel : public QStandardItemModel {
+    Q_OBJECT
+public:
+    enum Type { System, User, Both };
+    enum Roles {
+        TypeRole = Qt::UserRole,
+        IdRole,
+        ColorRole,
+        NameRole,
+        PinnedRole
+    };
+    explicit CategoryModel(Type type, QObject* parent = nullptr);
+public slots:
+    void refresh();
+
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    bool setData(const QModelIndex& index, const QVariant& val, int role = Qt::EditRole) override;
+
+    Qt::DropActions supportedDropActions() const override;
+    bool dropMimeData(const QMimeData* mimeData, Qt::DropAction action, int row, int column, const QModelIndex& parent) override;
+
+private:
+    Type m_type;
+};
+
+} // namespace ArcMeta
